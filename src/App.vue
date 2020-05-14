1ad05ed84176a6a38e7cdcfb51da39ca07f96ef6
zmy <111@qq.com>
2020-05-14 17:12:24 +0800

    dsa

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tab1>
+      <tab-item path="/home">
+        <img slot="tabImg" src="~assets/img/tabbar/home.svg" alt="">
+        <img slot="tabImg2" src="~assets/img/tabbar/home_active.svg">
+        <div slot="tabText">首页</div>
+      </tab-item>
+      <tab-item path="/category">
+        <img slot="tabImg" src="~assets/img/tabbar/category.svg" alt="">
+        <img slot="tabImg2" src="~assets/img/tabbar/category_active.svg">
+        <div slot="tabText">分类</div>
+      </tab-item>
+      <tab-item path="/cart" activ>
+        <img slot="tabImg" src="~assets/img/tabbar/cart.svg" alt="">
+        <img slot="tabImg2" src="~assets/img/tabbar/cart_active.svg" alt="">
+        <div slot="tabText">购物车</div>
+      </tab-item>
+      <tab-item path="/profile">
+        <img slot="tabImg" src="~assets/img/tabbar/profile.svg" alt="">
+        <img slot="tabImg2" src="~assets/img/tabbar/profile_active.svg">
+        <div slot="tabText">我的</div>
+      </tab-item>
+    </tab1>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  import tab1 from './components/common/tab/tab1';
+  import tabItem from "./components/common/tab/tabItem";
+  export default {
+   name: 'App',
+   components:{
+     tab1,
+     tabItem
+   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "assets/css/base.css";
+
 </style>
